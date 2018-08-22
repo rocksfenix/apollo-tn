23061287e0502fb9d850329725451e15b0bdeb43
Gerardo Gallegos <puxerop@gmail.com>
2018-08-22 10:32:53 -0700

    Se agrega redis como servicio para publicar y subscribir eventos

--- server/pupsub.js
+++ server/pupsub.js
@@ -1,16 +1,11 @@
-// import { PubSub } from 'graphql-subscriptions'
-import redis from 'redis'
+import Redis from 'ioredis'
 import { RedisPubSub } from 'graphql-redis-subscriptions'
 
-const client = redis.createClient({
-  url: 'redis://h:KbZ2EFlItNMRBm1mgxed4B96mjzgobIm@redis-17932.c8.us-east-1-4.ec2.cloud.redislabs.com:17932'
-})
+const REDIS_HOST = process.env.REDIS_HOST
 
 const pubsub = new RedisPubSub({
-  client
+  publisher: new Redis(REDIS_HOST),
+  subscriber: new Redis(REDIS_HOST)
 })
-// Eventualmente se usara graphql-redis-subscriptions
-// Ya es es mas escalable
-// pubsub.ee.setMaxListeners(30)
 
 export default pubsub
